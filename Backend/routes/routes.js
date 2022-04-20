f6858276b9487757129296285b482f8cb9cecd4f
Sahil <mydocs3000@gmail.com>
2022-04-20 13:40:12 +0530

    routes added with multer for csv upload

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -3,17 +3,17 @@ const productcontroller = require("../controllers/productcontroller");
 const usercontroller = require("../controllers/usercontroller");
 const authmiddleware = require("../middleware/authmiddleware");
 
-router.post("/addProduct", productcontroller.addProduct);
-router.post("/addUser", usercontroller.addUser);
-router.post("/loginUser", usercontroller.loginUser);
-router.post(
-  "/deleteUser/:id",
-  authmiddleware.verifytoken,
-  usercontroller.deleteUser
-);
-router.put("/updateProduct/:id", productcontroller.updateProduct);
-router.delete("/deleteProduct/:id/", productcontroller.deleteProduct);
-router.post("/getProduct/:QR_hashcode", productcontroller.readQR);
-router.post("/getallProducts/", productcontroller.readAllProducts);
+
+//Upload Handling
+const multer = require("multer");
+const csvUploader = multer({ dest: "uploads/" });
+
+//User Routes
+router.post("/signup", usercontroller.signup);
+router.post("/login", usercontroller.login);
+router.get("/users", authmiddleware.verifytoken, usercontroller.fetch_user_list);
+router.get("/users/:id", authmiddleware.verifytoken, usercontroller.fetch_user_details);
+router.get("/getallProducts/", authmiddleware.verifytoken, productcontroller.fetchproductlist);
+router.post("/products/upload", csvUploader.any(), authmiddleware.verifytoken, productcontroller.upload_products);
 
 module.exports = router;
