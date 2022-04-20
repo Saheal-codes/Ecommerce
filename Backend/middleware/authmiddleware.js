0b7e53805bb22540ec74585c90c11232e391996c
Sahil <mydocs3000@gmail.com>
2022-04-20 13:39:28 +0530

    auth middleware added

--- Backend/middleware/authmiddleware.js
+++ Backend/middleware/authmiddleware.js
@@ -21,10 +21,3 @@ exports.verifytoken = (req, res, next) => {
     });
   });
 };
-exports.isAdminverify = (req, res, next) => {
-  if (req.user.isAdmin) {
-    next();
-  } else {
-    res.status(403).send({ message: "Unauthorized" });
-  }
-};
